a9a519bc98b504fd2b3fc1665ac4cd6a5db3d278
shubham-cstechguru <shubham.cstechguru@gmail.com>
2021-03-20 21:39:52 +0530

    pricing and pages add view create

---
+++ app/Http/Requests/PageRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'page_title' => 'required|unique:pages',
+            'page_description' => 'required',
+            'page_image' => 'required|max:100|mimes:jpeg,jpg,png,gif',
+            'page_seo_title' => 'nullable',
+            'page_seo_description' => 'nullable',
+            'page_seo_keyword' => 'nullable',
+        ];
+    }
+}
